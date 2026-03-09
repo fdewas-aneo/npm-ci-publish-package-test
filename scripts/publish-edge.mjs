8494c43d0d5184fedb87664f15543b73c1831b5e
Faustin <fdewas@aneo.fr>
2026-03-09 10:31:04 +0100

    chore: test

--- scripts/publish-edge.mjs
+++ scripts/publish-edge.mjs
@@ -1,4 +1,4 @@
 import consola from 'consola'
 
 consola.log(`Publishing version...`);
-await $`pnpm publish --access public --no-git-checks --tag next`;
+await $`pnpm publish --access public --tag next`;
